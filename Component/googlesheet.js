d3b8eeb0ee45d44838b5032459e88a0d0b8ab1bb
SekiTaki <beetles1994klw@gmail.com>
2023-08-09 00:41:33 +0800

    sec push

--- Component/googlesheet.js
+++ Component/googlesheet.js
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import googlesheet from './Component/googlesheet';
 
-const App = () => {
+const MyComponent = () => {
   const [dateTime, setDateTime] = useState('');
 
+  const writeToSheet = async (dateTime) => {
+    // 上述程式碼放在此處
+
+    try {
+      const now = new Date();
+      const dateTimeString = now.toLocaleString();
+      await writeToSheet(dateTimeString);
+      console.log('已成功寫入 Google Sheets');
+    } catch (err) {
+      console.error('寫入 Google Sheets 時出錯:', err);
+    }
+  };
+
   const onPressButton = () => {
-    const now = new Date();
-    const dateTimeString = now.toLocaleString();
-    setDateTime(dateTimeString);
+    writeToSheet(dateTime);
   };
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>{dateTime}</Text>
       <Text>{dateTime}</Text>
       <TouchableOpacity
         style={{
@@ -26,11 +35,10 @@ const App = () => {
         }}
         onPress={onPressButton}
       >
-        <Text style={{color: 'white', fontWeight: 900}}>打卡</Text>
-        
+        <Text style={{ color: 'white', fontWeight: '900' }}>打卡</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
-export default App;
+export default MyComponent;
